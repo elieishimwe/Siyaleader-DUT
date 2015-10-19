5acbec465298ee7a89880325462450c7c0fcbacf
Elie Ishimwe <elieish@gmail.com>
2015-10-19 12:13:54 +0200

    updates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -186,7 +186,9 @@ class UserController extends Controller
         $user       = User::where('id',$request['userID'])->first();
         $role       = UserRole::where('slug','=',$request['role'])->first();
         $user->role = $role->id;
-        $user->name = $request['name'];
+        $title               = Title::where('slug','=',$request['title'])->first();
+        $user->title         = $title->id;
+
         $user->name = $request['name'];
         $user->updated_by = \Auth::user()->id;
         $user->updated_at =  \Carbon\Carbon::now('Africa/Johannesburg')->toDateTimeString();
