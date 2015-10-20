6b3ffb618adbb0eb2356c078995512f905b1ec92
Elie Ishimwe <elieish@gmail.com>
2015-10-20 12:22:24 +0200

    updates

--- resources/views/cases/profile.blade.php
+++ resources/views/cases/profile.blade.php
@@ -75,7 +75,7 @@
                                   </div>
 
                                   <div class="form-group">
-                                      {!! Form::label('Precinct', 'Precinct', array('class' => 'col-md-3 control-label')) !!}
+                                      {!! Form::label('Department', 'Department', array('class' => 'col-md-3 control-label')) !!}
                                       <div class="col-md-6">
                                         {!! Form::text('department',NULL,['class' => 'form-control input-sm','id' => 'department','disabled' => 'disabled']) !!}
 
