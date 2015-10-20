a536b6608fc395908b62e405f3df4700766355f1
Elie Ishimwe <elieish@gmail.com>
2015-10-20 13:23:42 +0200

    updates

--- app/Http/Requests/CaseStatusRequest.php
+++ app/Http/Requests/CaseStatusRequest.php
@@ -13,7 +13,7 @@ class CaseStatusRequest extends Request
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class CaseStatusRequest extends Request
     public function rules()
     {
         return [
-            //
+
+            'name'     =>'required|unique:cases_status',
+
         ];
     }
 }
