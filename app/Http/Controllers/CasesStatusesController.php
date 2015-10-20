e24576bdfb33a41db69abc0d69cba3ee7d62024f
Elie Ishimwe <elieish@gmail.com>
2015-10-20 13:40:55 +0200

    updates

--- app/Http/Controllers/CasesStatusesController.php
+++ app/Http/Controllers/CasesStatusesController.php
@@ -68,9 +68,11 @@ class CasesStatusesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($id,CaseStatus $caseStatus)
     {
-        //
+
+        $case    = CaseStatus::where('id',$id)->first();
+        return [$case];
     }
 
     /**
