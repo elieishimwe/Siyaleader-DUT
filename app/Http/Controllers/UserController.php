73f3ee97b4970d9e74034c515fb2cfac82bc3d72
Elie Ishimwe <elieish@gmail.com>
2015-10-23 14:57:38 +0200

    udpates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -202,9 +202,9 @@ class UserController extends Controller
         $user->municipality  = $municipality->id;
         $ward                = Ward::where('slug','=',$request['ward'])->first();
         $user->ward          = $ward->id;
-        $user->area       = $request['area'];
-        $user->updated_by = \Auth::user()->id;
-        $user->updated_at =  \Carbon\Carbon::now('Africa/Johannesburg')->toDateTimeString();
+        $user->area          = $request['area'];
+        $user->updated_by    = \Auth::user()->id;
+        $user->updated_at    =  \Carbon\Carbon::now('Africa/Johannesburg')->toDateTimeString();
         $user->save();
         \Session::flash('success', 'well done! User '.$request['name'].' has been successfully updated!');
         return redirect()->back();
@@ -227,7 +227,8 @@ class UserController extends Controller
             ->join('positions','users.position','=','positions.id')
             ->join('municipalities','users.municipality','=','municipalities.id')
             ->join('wards','users.ward','=','wards.id')
-            ->whereRaw("CONCAT(`users`.`name`, ' ', `users`.`surname`, ' ', `users`.`cellphone`) LIKE '%{$searchString}%'")
+            ->whereRaw(
+                            "CONCAT(`users`.`name`, ' ', `users`.`surname`, ' ', `users`.`cellphone`) LIKE '%{$searchString}%'")
             ->select(
                         array
 
@@ -256,21 +257,21 @@ class UserController extends Controller
        foreach ($users as $user) {
 
             $data[] = array(
-                                "name"=>"{$user->name} > {$user->surname} > {$user->cellphone}",
-                                "id" =>"{$user->id}",
-                                "hseName" => "{$user->name}",
-                                "hseSurname" => "{$user->surname}",
-                                "hseIdNumber" => "{$user->id_number}",
-                                "hseCellphone" => "{$user->cellphone}",
-                                "hseLanguage" => "{$user->language}",
-                                "hseProvince" => "{$user->province}",
-                                "hseDistrict" => "{$user->district}",
-                                "hseMunicipality" => "{$user->municipality}",
-                                "hseWard" => "{$user->ward}",
-                                "hseArea" =>"{$user->area}",
-                                "hseNumber" =>"{$user->house_number}",
-                                "hseTitle" => "{$user->title}",
-                                "hsePosition" => "{$user->position}"
+                                "name"              =>"{$user->name} > {$user->surname} > {$user->cellphone}",
+                                "id"                =>"{$user->id}",
+                                "hseName"           => "{$user->name}",
+                                "hseSurname"        => "{$user->surname}",
+                                "hseIdNumber"       => "{$user->id_number}",
+                                "hseCellphone"      => "{$user->cellphone}",
+                                "hseLanguage"       => "{$user->language}",
+                                "hseProvince"       => "{$user->province}",
+                                "hseDistrict"       => "{$user->district}",
+                                "hseMunicipality"   => "{$user->municipality}",
+                                "hseWard"           => "{$user->ward}",
+                                "hseArea"           =>"{$user->area}",
+                                "hseNumber"         =>"{$user->house_number}",
+                                "hseTitle"          => "{$user->title}",
+                                "hsePosition"       => "{$user->position}"
                             );
        }
 
